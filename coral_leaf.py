1b8d41aa2205932edc84fab3e16a960f606f2c20
alireza panahi <148740403+alireza-panahi@users.noreply.github.com>
2025-04-18 18:46:08 +0330

    تمرین صخره مرجانی

---
+++ coral_leaf.py
@@ -0,0 +1,34 @@
+name: str='dr.ardestani---alireza-panahi'
+print(f'{name:_^50}')
+import turtle as T
+import random as R
+tree=T.Turtle()
+screen=T.Screen()
+screen.setup(width=900,height=600)
+scale = 50
+screen.setworldcoordinates(-5,-1,10,11)
+T.write(name)
+tree.color('coral')
+y=0
+x=0
+for i in range (15000):
+    r=R.random()
+    if r < 0.4:
+        x_new = 0.31*x - 0.53*y + 0.89
+        y_new =-0.46*x - 0.29*y + 1.04
+    elif r < 0.55:
+        x_new = 0.31*x - 0.08*y + .89
+        y_new = 0.15*x - 0.45*y + 0.34
+    else:
+        x_new = 0.55*y + 0.01
+        y_new = 0.69*x - 0.20*y + 0.38
+    x=x_new
+    y=y_new
+    T.tracer(0)
+    tree.speed(0)
+    tree.penup()
+    tree.goto(x*5,y*5)
+    tree.dot(2)
+    # print(r)
+
+T.done()
